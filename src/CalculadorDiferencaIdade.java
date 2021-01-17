c3e82e3c2dc763e2f20d86eb8fdf325638a0cd94
wellbigas <77459233+wellbigas@users.noreply.github.com>
2021-01-17 16:23:41 -0300

    Initial commit

---
+++ src/CalculadorDiferencaIdade.java
@@ -0,0 +1,10 @@
+public class CalculadorDiferencaIdade {
+        public static void main(String[] args) {
+            int firstAge = 26;
+            int secondAge = 22;
+            int ageDifferent = firstAge - secondAge;
+            System.out.println("A diferença entre as idades é =" + ageDifferent);
+
+        }
+
+    }
