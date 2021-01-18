28e94bb6396e59244bd0379a90df7dab0b9dd7c1
wellbigas <77459233+wellbigas@users.noreply.github.com>
2021-01-18 10:55:53 -0300

    Update CalculadorDiferencaIdade.java

--- src/CalculadorDiferencaIdade.java
+++ src/CalculadorDiferencaIdade.java
@@ -4,6 +4,16 @@ public class CalculadorDiferencaIdade {
             int secondAge = 22;
             int ageDifferent = firstAge - secondAge;
             System.out.println("A diferença entre as idades é =" + ageDifferent);
+             
+            /*
+              Lendo informações com Scanner
+            */
+                
+            Scanner scanner = new Scanner (System.in);
+                double distanciaPercorrida = scanner.nextDouble ();
+                scanner.nclose();
+                
+                
 
         }
 
